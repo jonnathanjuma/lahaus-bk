6dc80e33e10b8e229ed84c012b0549ba7e3c6637
Jonnathan Juma <jjuma@favorita.com>
2022-05-10 15:53:12 -0500

    cambios test

--- src/test/java/com/lahaus/demo/DemoApplicationTests.java
+++ src/test/java/com/lahaus/demo/DemoApplicationTests.java
@@ -3,38 +3,28 @@ package com.lahaus.demo;
 import com.lahaus.demo.controller.Controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@AutoConfigureMockMvc
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Autowired
 	private Controller controller;
-	@LocalServerPort
-	private int port;
+
 	@Test
 	public void contextLoads() throws Exception {
 		assertThat(controller).isNotNull();
 	}
 
-	@Autowired
-	private MockMvc mockMvc;
-
-	@Test
-	public void shouldReturnDefaultMessage() throws Exception {
-		this.mockMvc.perform(get("/existe")).andDo(print()).andExpect(status().isOk())
-				.andExpect(content().string(containsString("existe")));
-	}
+//	@Autowired
+//	private MockMvc mockMvc;
+//
+//	@Test
+//	public void shouldReturnDefaultMessage() throws Exception {
+//		this.mockMvc.perform(get("/existe")).andDo(print()).andExpect(status().isOk())
+//				.andExpect(content().string(containsString("existe")));
+//	}
 
 }
